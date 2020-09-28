d6dd5908817715a486ef16c4b6be8dc62c00bdae
Diego Rueda <ing.diegorueda@gmail.com>
2020-09-28 16:56:22 -0500

    setup config file, modify write to disk methods

--- src/data_download.py
+++ src/data_download.py
@@ -1,5 +1,6 @@
 from Bio import Entrez, SeqIO
 from Bio.SeqRecord import SeqRecord
+import configparser
 import pandas as pd
 import os
 import csv
@@ -17,7 +18,6 @@ def fetch_using_id(accesion, db):
         SeqIO Record:  standard Sequence Input/Output interface for BioPython contains all data obtained from database
     """
     # Initialize parameters
-    db = db
     paramEutils = {'usehistory': 'y'}
 
     # Generate query to Entrez search
@@ -40,67 +40,72 @@ def search_ncib(term, db='nucleotide', retmax=20):
     with Entrez.esearch(db=db, retmax=retmax, term=term,
                         idtype='acc') as handle:
         results = Entrez.read(handle)
+        accesion_list = results['IdList']
+    return accesion_list
 
-    accesion_list = results['IdList']
-    sequence_list = []
+
+def write_sequences_to_disk(items, database, output_dir='output'):
 
     confirm = input(
-        f'You are about to download {len(accesion_list)} files.\n Do you want to continue? [Y/n] '
+        f'You are about to download {len(items)} files.\n Do you want to continue? [Y/n] '
     )
     if confirm in ('y', 'Y'):
-        for accesion in tqdm(accesion_list):
-            sequence_list.append(fetch_using_id(accesion, db))
-    return sequence_list
 
+        # Fetch all individual items from database
+        sequences = []
+        for accesion in tqdm(items):
+            sequences.append(fetch_using_id(accesion, database))
 
-def write_sequences_to_disk(sequences, output_dir='output'):
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
 
-    columns = [
-        'id', 'description', 'origin', 'country', 'state', 'molecule_type',
-        'topology', 'date', 'taxonomy'
-    ]
-    meta = pd.DataFrame(columns=columns)
+        columns = [
+            'id', 'description', 'origin', 'country', 'state', 'molecule_type',
+            'topology', 'date', 'taxonomy'
+        ]
+        meta = pd.DataFrame(columns=columns)
 
-    for seq in sequences:
-        # Write fasta file with id and sequence
-        record = SeqRecord(seq.seq, f'{seq.id}', '', '')
-        SeqIO.write(record, f'{output_dir}/{seq.id}.fasta', 'fasta')
+        for seq in sequences:
+            # Write fasta file with id and sequence
+            record = SeqRecord(seq.seq, f'{seq.id}', '', '')
+            SeqIO.write(record, f'{output_dir}/{seq.id}.fasta', 'fasta')
 
-        # Store meta data in dataframe
-        _id = seq.id
-        desc, origin, country, country_extra, _ = seq.description.split('/')
-        molecule_type = seq.annotations['molecule_type']
-        topology = seq.annotations['topology']
-        date = seq.annotations['date']
-        taxonomy = '_'.join(seq.annotations['taxonomy'])
+            # Append meta data in dataframe
+            _id = seq.id
+            desc, origin, country, country_extra, _ = seq.description.split(
+                '/')
+            molecule_type = seq.annotations['molecule_type']
+            topology = seq.annotations['topology']
+            date = seq.annotations['date']
+            taxonomy = '_'.join(seq.annotations['taxonomy'])
 
-        data = pd.Series((_id, desc, origin, country, country_extra,
-                          molecule_type, topology, date, taxonomy),
-                         index=meta.columns)
+            data = pd.Series((_id, desc, origin, country, country_extra,
+                              molecule_type, topology, date, taxonomy),
+                             index=meta.columns)
 
-        meta = meta.append(data, ignore_index='True')
+            meta = meta.append(data, ignore_index='True')
 
-    meta.to_csv('meta.csv', index=False)
+        # Save dataframe to file
+        meta.to_csv('meta.csv', index=False)
+        print('Finished.')
+
+    else:
+        print('No files downloaded...Closing.')
 
 
 if __name__ == "__main__":
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
     # Always tell who you are to NCIB
-    email = 'druedaplata@protonmail.com'
+    email = config.get('DEFAULT', 'Email')
     Entrez.email = email
+    database = config.get('DEFAULT', 'Database')
+    search_query = config.get('DEFAULT', 'Search_Query')
+    retmax = config.get('DEFAULT', 'Max_Items_Retreived')
 
-    db = 'nucleotide'
-    search_query = '(("Severe acute respiratory syndrome coronavirus 2"[Organism] OR sars cov 2[All Fields]) AND genome[All Fields] AND complete[All Fields]) AND ("2020/01/01"[PDAT] : "2020/04/31"[PDAT])'
-
-    # get all ids to download
-
-    sequences = search_ncib(search_query, db, retmax=2)
-    print('downloaded')
-    # write sequences to disk
-
-    write_sequences_to_disk(sequences)
-
-    # fetch all ids
-
-    # save all ids to csv file
+    # Search all items in query
+    items = search_ncib(search_query, database, retmax=retmax)
+    # Write all items in disk, if user wants.
+    write_sequences_to_disk(items, database)
