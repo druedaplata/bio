c9cdca9e9878262e239662025906655871a0d41f
Diego Rueda <ing.diegorueda@gmail.com>
2020-09-29 15:18:08 -0500

    download all sequences from an input year

--- src/data_download.py
+++ src/data_download.py
@@ -47,76 +47,75 @@ def search_ncib(term, db='nucleotide', retmax=20):
 
 def write_sequences_to_disk(items, database, output_dir):
 
-    confirm = input(
-        f'You are about to download {len(items)} files.\n Do you want to continue? [Y/n] '
-    )
-    if confirm in ('y', 'Y'):
+    # Fetch all individual items from database
 
-        # Fetch all individual items from database
-        sequences = []
-        for accesion in tqdm(items):
-            sequences.append(fetch_using_id(accesion, database))
+    sequences = []
+    for accesion in tqdm(items):
+        sequences.append(fetch_using_id(accesion, database))
 
-        if not os.path.exists(output_dir):
-            os.mkdir(output_dir)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
 
-        if not os.path.exists(f'{output_dir}/seqs'):
-            os.mkdir(f'{output_dir}/seqs')
+    if not os.path.exists(f'{output_dir}/seqs'):
+        os.mkdir(f'{output_dir}/seqs')
 
-        columns = [
-            'id', 'organism', 'host', 'country', 'isolate', 'isolation_source',
-            'molecule_type', 'dbxref', 'topology', 'taxonomy',
-            'collection_date', 'reported_date', 'n_count'
-        ]
-        meta = pd.DataFrame(columns=columns)
+    columns = [
+        'id', 'organism', 'host', 'country', 'isolate', 'isolation_source',
+        'molecule_type', 'dbxref', 'topology', 'taxonomy', 'collection_date',
+        'reported_date', 'n_count'
+    ]
+    meta = pd.DataFrame(columns=columns)
 
-        records = []
-        for seq in sequences:
+    records = []
+    for seq in sequences:
 
-            # Check for N in each sequence
-            max_len = len(seq.seq)
-            n_count = np.sum(np.array(seq.seq) == 'N')
-            print(f'%N = {n_count/max_len}')
+        # Check for N in each sequence
+        max_len = len(seq.seq)
+        n_count = np.sum(np.array(seq.seq) == 'N')
 
-            # Write fasta file with id and sequence
-            record = SeqRecord(seq.seq, f'{seq.id}', '', '')
-            SeqIO.write(record, f'{output_dir}/seqs/{seq.id}.fasta', 'fasta')
-            records.append(record)
+        # Write fasta file with id and sequence
+        record = SeqRecord(seq.seq, f'{seq.id}', '', '')
+        SeqIO.write(record, f'{output_dir}/seqs/{seq.id}.fasta', 'fasta')
+        records.append(record)
 
-            # Append meta data in dataframe
-            _id = seq.id
-            organism = seq.features[0].qualifiers.get('organism', ['NaN'])[0]
-            host = seq.features[0].qualifiers.get('host', ['NaN'])[0]
-            country = seq.features[0].qualifiers.get('country', ['NaN'])[0]
-            isolate = seq.features[0].qualifiers.get('isolate', ['NaN'])[0]
-            isolation_source = seq.features[0].qualifiers.get(
-                'isolation_source', ['NaN'])[0]
-            molecule_type = seq.features[0].qualifiers.get(
-                'mol_type', ['NaN'])[0]
-            dbxref = seq.features[0].qualifiers.get('db_xref', ['NaN'])[0]
-            collection_date = seq.features[0].qualifiers.get(
-                'collection_date', ['NaN'])[0]
-            topology = seq.annotations['topology']
-            reported_date = seq.annotations['date']
-            taxonomy = '|'.join(seq.annotations['taxonomy'])
+        # Append meta data in dataframe
+        _id = seq.id
+        organism = seq.features[0].qualifiers.get('organism', ['NaN'])[0]
+        host = seq.features[0].qualifiers.get('host', ['NaN'])[0]
+        country = seq.features[0].qualifiers.get('country', ['NaN'])[0]
+        isolate = seq.features[0].qualifiers.get('isolate', ['NaN'])[0]
+        isolation_source = seq.features[0].qualifiers.get(
+            'isolation_source', ['NaN'])[0]
+        molecule_type = seq.features[0].qualifiers.get('mol_type', ['NaN'])[0]
+        dbxref = seq.features[0].qualifiers.get('db_xref', ['NaN'])[0]
+        collection_date = seq.features[0].qualifiers.get(
+            'collection_date', ['NaN'])[0]
+        topology = seq.annotations['topology']
+        reported_date = seq.annotations['date']
+        taxonomy = '|'.join(seq.annotations['taxonomy'])
 
-            data = pd.Series(
-                (_id, organism, host, country, isolate, isolation_source,
-                 molecule_type, dbxref, topology, taxonomy, collection_date,
-                 reported_date, n_count),
-                index=meta.columns)
+        data = pd.Series((_id, organism, host, country, isolate,
+                          isolation_source, molecule_type, dbxref, topology,
+                          taxonomy, collection_date, reported_date, n_count),
+                         index=meta.columns)
 
-            meta = meta.append(data, ignore_index='True')
+        meta = meta.append(data, ignore_index='True')
 
-        # Save all sequences in a single file
-        SeqIO.write(records, f'{output_dir}/sequences.fasta', 'fasta')
+    # Save all sequences in a single file
+    SeqIO.write(records, f'{output_dir}/sequences.fasta', 'fasta')
 
-        # Save dataframe to file
-        meta.to_csv(f'{output_dir}/meta.csv', index=False)
-        print('Finished.')
+    # Save dataframe to file
+    meta.to_csv(f'{output_dir}/meta.csv', index=False)
 
-    else:
-        print('No files downloaded...Closing.')
+
+def download(month, year, search_query, retmax):
+    month_k, month_v = month
+    current_search_query = f'{search_query} AND ("{publication_year}/{month_k}/01"[PDAT] : "{publication_year}/{month_k}/31"[PDAT])'
+    output_dir = f'{month_v}_{publication_year}'
+    # Search all items in query
+    items = search_ncib(current_search_query, database, retmax=retmax)
+    # Write all items in disk, if user wants.
+    write_sequences_to_disk(items, database, output_dir)
 
 
 if __name__ == "__main__":
@@ -130,14 +129,22 @@ if __name__ == "__main__":
     database = config.get('DEFAULT', 'Database')
     search_query = config.get('DEFAULT', 'Search_Query')
     retmax = config.get('DEFAULT', 'Max_Items_Retreived')
-    output_dir = config.get('DEFAULT', 'Output_Dir')
+    publication_year = config.get('DEFAULT', 'Publication_Year')
 
-    pub_start_date = config.get('DEFAULT', 'Pub_Start_Date')
-    pub_end_date = config.get('DEFAULT', 'Pub_End_Date')
+    month_dict = {
+        '01': 'Enero',
+        '02': 'Febrero',
+        '03': 'Marzo',
+        '04': 'Abril',
+        '05': 'Mayo',
+        '06': 'Junio',
+        '07': 'Julio',
+        '08': 'Agosto',
+        '09': 'Septiembre',
+        '10': 'Octubre',
+        '11': 'Noviembre',
+        '12': 'Diciembre',
+    }
 
-    search_query += f'AND ({pub_start_date}[PDAT] : {pub_end_date}[PDAT])'
-
-    # Search all items in query
-    items = search_ncib(search_query, database, retmax=retmax)
-    # Write all items in disk, if user wants.
-    write_sequences_to_disk(items, database, output_dir)
+    for month in month_dict.items():
+        download(month, publication_year, search_query, retmax)
