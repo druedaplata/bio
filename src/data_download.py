ce44b4dd55836807b9c27a5e87cd804d01db6441
Diego Rueda <ing.diegorueda@gmail.com>
2020-09-30 11:40:03 -0500

    include several years search, n_count filter added to under 1 percent sequences and only homo sapiens data

--- src/data_download.py
+++ src/data_download.py
@@ -4,7 +4,6 @@ import configparser
 import pandas as pd
 import numpy as np
 import os
-import csv
 from tqdm import tqdm
 
 
@@ -72,34 +71,42 @@ def write_sequences_to_disk(items, database, output_dir):
         # Check for N in each sequence
         max_len = len(seq.seq)
         n_count = np.sum(np.array(seq.seq) == 'N')
+        n_percent = n_count / max_len
 
-        # Write fasta file with id and sequence
-        record = SeqRecord(seq.seq, f'{seq.id}', '', '')
-        SeqIO.write(record, f'{output_dir}/seqs/{seq.id}.fasta', 'fasta')
-        records.append(record)
+        if n_percent < 0.1:
+            # Append meta data in dataframe
+            _id = seq.id
+            organism = seq.features[0].qualifiers.get('organism', ['NaN'])[0]
+            host = seq.features[0].qualifiers.get('host', ['NaN'])[0]
 
-        # Append meta data in dataframe
-        _id = seq.id
-        organism = seq.features[0].qualifiers.get('organism', ['NaN'])[0]
-        host = seq.features[0].qualifiers.get('host', ['NaN'])[0]
-        country = seq.features[0].qualifiers.get('country', ['NaN'])[0]
-        isolate = seq.features[0].qualifiers.get('isolate', ['NaN'])[0]
-        isolation_source = seq.features[0].qualifiers.get(
-            'isolation_source', ['NaN'])[0]
-        molecule_type = seq.features[0].qualifiers.get('mol_type', ['NaN'])[0]
-        dbxref = seq.features[0].qualifiers.get('db_xref', ['NaN'])[0]
-        collection_date = seq.features[0].qualifiers.get(
-            'collection_date', ['NaN'])[0]
-        topology = seq.annotations['topology']
-        reported_date = seq.annotations['date']
-        taxonomy = '|'.join(seq.annotations['taxonomy'])
+            if host.lower() == 'homo sapiens':
 
-        data = pd.Series((_id, organism, host, country, isolate,
-                          isolation_source, molecule_type, dbxref, topology,
-                          taxonomy, collection_date, reported_date, n_count),
-                         index=meta.columns)
+                country = seq.features[0].qualifiers.get('country', ['NaN'])[0]
+                isolate = seq.features[0].qualifiers.get('isolate', ['NaN'])[0]
+                isolation_source = seq.features[0].qualifiers.get(
+                    'isolation_source', ['NaN'])[0]
+                molecule_type = seq.features[0].qualifiers.get(
+                    'mol_type', ['NaN'])[0]
+                dbxref = seq.features[0].qualifiers.get('db_xref', ['NaN'])[0]
+                collection_date = seq.features[0].qualifiers.get(
+                    'collection_date', ['NaN'])[0]
+                topology = seq.annotations['topology']
+                reported_date = seq.annotations['date']
+                taxonomy = '|'.join(seq.annotations['taxonomy'])
 
-        meta = meta.append(data, ignore_index='True')
+                data = pd.Series(
+                    (_id, organism, host, country, isolate, isolation_source,
+                     molecule_type, dbxref, topology, taxonomy,
+                     collection_date, reported_date, n_count),
+                    index=meta.columns)
+
+                meta = meta.append(data, ignore_index='True')
+
+                # Write fasta file with id and sequence
+                record = SeqRecord(seq.seq, f'{seq.id}', '', '')
+                SeqIO.write(record, f'{output_dir}/seqs/{seq.id}.fasta',
+                            'fasta')
+                records.append(record)
 
     # Save all sequences in a single file
     SeqIO.write(records, f'{output_dir}/sequences.fasta', 'fasta')
@@ -109,13 +116,13 @@ def write_sequences_to_disk(items, database, output_dir):
 
 
 def download(month, year, search_query, retmax):
-    month_k, month_v = month
-    current_search_query = f'{search_query} AND ("{publication_year}/{month_k}/01"[PDAT] : "{publication_year}/{month_k}/31"[PDAT])'
-    output_dir = f'{month_v}_{publication_year}'
+    current_search_query = f'{search_query} AND ("{year}/{month}/01"[PDAT] : "{year}/{month}/31"[PDAT])'
+    output_dir = f'{month}_{year}'
     # Search all items in query
     items = search_ncib(current_search_query, database, retmax=retmax)
     # Write all items in disk, if user wants.
-    write_sequences_to_disk(items, database, output_dir)
+    if len(items) > 0:
+        write_sequences_to_disk(items, database, output_dir)
 
 
 if __name__ == "__main__":
@@ -129,22 +136,12 @@ if __name__ == "__main__":
     database = config.get('DEFAULT', 'Database')
     search_query = config.get('DEFAULT', 'Search_Query')
     retmax = config.get('DEFAULT', 'Max_Items_Retreived')
-    publication_year = config.get('DEFAULT', 'Publication_Year')
+    publication_years = config.get('DEFAULT', 'Publication_Years').split(',')
 
-    month_dict = {
-        '01': 'Enero',
-        '02': 'Febrero',
-        '03': 'Marzo',
-        '04': 'Abril',
-        '05': 'Mayo',
-        '06': 'Junio',
-        '07': 'Julio',
-        '08': 'Agosto',
-        '09': 'Septiembre',
-        '10': 'Octubre',
-        '11': 'Noviembre',
-        '12': 'Diciembre',
-    }
+    months = ('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+              '12')
 
-    for month in month_dict.items():
-        download(month, publication_year, search_query, retmax)
+    for year in publication_years:
+        for month in months:
+            print(f'{month}_{year}')
+            download(month, year, search_query, retmax)
