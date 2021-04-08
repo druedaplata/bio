7656ba558be18495fd78bc6a44741b04019b337e
Diego Rueda GUANE <ing.diegorueda@gmail.com>
2021-04-08 16:26:09 -0500

    scripts downloads from last interruption point

--- src/data_download.py
+++ src/data_download.py
@@ -47,74 +47,82 @@ def search_ncib(term, db='nucleotide', retmax=20):
 
 def write_sequences_to_disk(items, database, output_dir):
 
-    # Fetch all individual items from database
-
-    sequences = []
-    for accesion in tqdm(items):
-        sequences.append(fetch_using_id(accesion, database))
-        time.sleep(0.5)
-
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    if not os.path.exists(f'{output_dir}/seqs'):
-        os.mkdir(f'{output_dir}/seqs')
-
+    # Read Files from previous runs
     columns = [
         'id', 'organism', 'host', 'country', 'isolate', 'isolation_source',
         'molecule_type', 'dbxref', 'topology', 'taxonomy', 'collection_date',
         'reported_date', 'n_count'
     ]
     meta = pd.DataFrame(columns=columns)
+    if os.path.exists(f'{output_dir}/meta.csv'):
+        meta = pd.read_csv(f'{output_dir}/meta.csv', index_col=None)
 
     records = []
-    for seq in sequences:
+    if os.path.exists(f'{output_dir}/sequences.fasta'):
+        records = list(SeqIO.parse(f'{output_dir}/sequences.fasta', "fasta"))
 
-        # Check for N in each sequence
-        max_len = len(seq.seq)
-        n_count = np.sum(np.array(seq.seq) == 'N')
-        n_percent = n_count / max_len
+    items_in_disk = []
+    if os.path.exists(f'{output_dir}/seqs'):
+        items_in_disk = os.listdir(f'{output_dir}/seqs')
 
-        if n_percent < 0.1:
-            # Append meta data in dataframe
-            _id = seq.id
-            organism = seq.features[0].qualifiers.get('organism', ['NaN'])[0]
-            host = seq.features[0].qualifiers.get('host', ['NaN'])[0]
+    assert len(records) == len(items_in_disk)
+    print(f'{len(items_in_disk)} of {len(items)} in disk.')
 
-            if host.lower() == 'homo sapiens':
+    # Create Output folders
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    if not os.path.exists(f'{output_dir}/seqs'):
+        os.mkdir(f'{output_dir}/seqs')
+    
+    
+    try:
+        for accesion in tqdm(items):
+            # Ignore files if already in disk
+            if f'{accesion}.fasta' in items_in_disk:
+                continue
+            seq = fetch_using_id(accesion, database)
+            time.sleep(0.1)
 
-                country = seq.features[0].qualifiers.get('country', ['NaN'])[0]
-                isolate = seq.features[0].qualifiers.get('isolate', ['NaN'])[0]
-                isolation_source = seq.features[0].qualifiers.get(
-                    'isolation_source', ['NaN'])[0]
-                molecule_type = seq.features[0].qualifiers.get(
-                    'mol_type', ['NaN'])[0]
-                dbxref = seq.features[0].qualifiers.get('db_xref', ['NaN'])[0]
-                collection_date = seq.features[0].qualifiers.get(
-                    'collection_date', ['NaN'])[0]
-                topology = seq.annotations['topology']
-                reported_date = seq.annotations['date']
-                taxonomy = '|'.join(seq.annotations['taxonomy'])
+            # Check for N tolerance 
+            n_count = np.sum(np.array(seq.seq) == 'N')
+            if n_count == 0:
+                # Append meta data in dataframe
+                _id = seq.id
+                organism = seq.features[0].qualifiers.get('organism', ['NaN'])[0]
+                host = seq.features[0].qualifiers.get('host', ['NaN'])[0]
 
-                data = pd.Series(
-                    (_id, organism, host, country, isolate, isolation_source,
-                     molecule_type, dbxref, topology, taxonomy,
-                     collection_date, reported_date, n_count),
-                    index=meta.columns)
+                if host.lower() == 'homo sapiens':
+                    country = seq.features[0].qualifiers.get('country', ['NaN'])[0]
+                    isolate = seq.features[0].qualifiers.get('isolate', ['NaN'])[0]
+                    isolation_source = seq.features[0].qualifiers.get('isolation_source', ['NaN'])[0]
+                    molecule_type = seq.features[0].qualifiers.get('mol_type', ['NaN'])[0]
+                    dbxref = seq.features[0].qualifiers.get('db_xref', ['NaN'])[0]
+                    collection_date = seq.features[0].qualifiers.get('collection_date', ['NaN'])[0]
+                    topology = seq.annotations['topology']
+                    reported_date = seq.annotations['date']
+                    taxonomy = '|'.join(seq.annotations['taxonomy'])
 
-                meta = meta.append(data, ignore_index='True')
+                    data = pd.Series(
+                        (_id, organism, host, country, isolate, isolation_source,
+                        molecule_type, dbxref, topology, taxonomy,
+                        collection_date, reported_date, n_count),
+                        index=meta.columns)
 
-                # Write fasta file with id and sequence
-                record = SeqRecord(seq.seq, f'{seq.id}', '', '')
-                SeqIO.write(record, f'{output_dir}/seqs/{seq.id}.fasta',
-                            'fasta')
-                records.append(record)
+                    meta = meta.append(data, ignore_index='True')
 
-    # Save all sequences in a single file
-    SeqIO.write(records, f'{output_dir}/sequences.fasta', 'fasta')
+                    # Write fasta file with id and sequence
+                    record = SeqRecord(seq.seq, f'{seq.id}', '', '')
+                    SeqIO.write(record, f'{output_dir}/seqs/{seq.id}.fasta',
+                                'fasta')
+                    records.append(record)
+    finally:
+        # Save all sequences in a single file
+        SeqIO.write(records, f'{output_dir}/sequences.fasta', 'fasta')
 
-    # Save dataframe to file
-    meta.to_csv(f'{output_dir}/meta.csv', index=False)
+        # Save dataframe to file
+        meta.to_csv(f'{output_dir}/meta.csv', index=False)
+
+    
 
 
 def download(month, year, search_query, retmax):
@@ -122,6 +130,7 @@ def download(month, year, search_query, retmax):
     output_dir = f'{month}_{year}'
     # Search all items in query
     items = search_ncib(current_search_query, database, retmax=retmax)
+
     # Write all items in disk, if user wants.
     if len(items) > 0:
         write_sequences_to_disk(items, database, output_dir)
